7edfe70db5c3fe87dd7196fa3391934ad1e1dafe
shylit <trisvetovrichard@gmail.com>
2026-04-26 15:40:17 +0300

    создаёт и настраивает HTTP-клиент (Ktor)

---
+++ NobelApp2-mod/data/remote/KtorClientFactory.kt
@@ -0,0 +1,30 @@
+//---
+package com.example.nobelapp.data.remote
+
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.android.Android
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logging
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
+
+object KtorClientFactory {
+
+    fun create(): HttpClient {
+        return HttpClient(Android) {
+            install(ContentNegotiation) {
+                json(
+                    Json {
+                        ignoreUnknownKeys = true
+                        isLenient = true
+                    }
+                )
+            }
+
+            install(Logging) {
+                level = LogLevel.ALL
+            }
+        }
+    }
+}
